fb8c1d93626ab33f48d0af9d85d800cf40d4ae13
SJ Chou <sj@toright.com>
2017-10-05 15:56:43 +0800

    Update mysql_init.sql

--- conf/mysql_init.sql
+++ conf/mysql_init.sql
@@ -9,3 +9,5 @@ DELETE FROM `db` WHERE `user` = '<MYSQL_USER_NAME>';
 INSERT INTO `db` (`Host`, `Db`, `User`, `Select_priv`, `Insert_priv`, `Update_priv`, `Delete_priv`, `Create_priv`, `Drop_priv`, `Grant_priv`, `References_priv`, `Index_priv`, `Alter_priv`, `Create_tmp_table_priv`, `Lock_tables_priv`, `Create_view_priv`, `Show_view_priv`, `Create_routine_priv`, `Alter_routine_priv`, `Execute_priv`, `Event_priv`, `Trigger_priv`) VALUES ('%', '<MYSQL_USER_NAME>', '<MYSQL_USER_NAME>', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
 
 FLUSH PRIVILEGES;
+
+CREATE DATABASE <MYSQL_USER_NAME>;
